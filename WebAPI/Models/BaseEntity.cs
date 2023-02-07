8636944dfdb5427dad82c46e964afc5ead944f0c
deb240485 <deb240485rana@gmail.com>
2023-02-07 14:37:05 +0530

    WebAPI: Added property related entities to database.

---
+++ WebAPI/Models/BaseEntity.cs
@@ -0,0 +1,11 @@
+namespace WebAPI.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime LastUpdatedOn { get; set; } = DateTime.UtcNow;
+
+        public int LastUpdatedBy { get; set; }
+    }
+}
